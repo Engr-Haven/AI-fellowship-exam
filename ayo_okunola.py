474d5cc28a2686534b50ce4b8dee897bf6c565eb
Ayobamidele Okunola <thehavencreation@gmail.com>
2025-09-04 15:38:40 +0100

    AI Python Exam

---
+++ ayo_okunola.py
@@ -0,0 +1,40 @@
+#Question 1 >>>
+#x = int(input())
+#y = int(input())
+
+#def calc():
+ #   if x == int(input()):
+  #      print()
+
+
+#Question 2 >>>
+while True:
+    user_input = input("Enter a number (or type 'exit' to quit): ")
+    if user_input == "exit":
+        print("Goodbye!")
+        break        # break out of loop
+    
+    num = int((user_input))   # convert to integer
+    
+    if num % 2 == 0:
+        print("The number is even")
+    else:
+        print("The number is odd")
+
+
+#Question 3 >>>
+
+while True:
+    exit = str("exit")
+    age = int(input("Enter your age (or type exit to quit): "))
+    if age == exit:
+        print("Goodbye!")
+        break
+    
+    try:
+        if age >= 18:
+            print("You can vote")
+        else:
+            print("You cannot vote")
+    except:
+        print("Invalid input")
